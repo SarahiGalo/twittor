f7e602ba7dc587107b9fd73a9f68be8c0155a8d2
Sarahi Galo <al>
2019-12-07 19:07:34 -0600

    Cache con Network Fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const STATIC_CACHE = 'static-v1';
+//imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -12,7 +15,8 @@ const APP_SHELL = [
   'img/avatars/spiderman.jpg',
   'img/avatars/thor.jpg',
   'img/avatars/wolverine.jpg',
-  'js/app.js'
+  'js/app.js',
+  'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -29,7 +33,7 @@ self.addEventListener('install', e => {
   cache.addAll(APP_SHELL));
 
   const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
-  cache.addAll(INMUTABLE_CACHE));
+  cache.addAll(APP_SHELL_INMUTABLE));
 
   e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
@@ -45,4 +49,17 @@ self.addEventListener('activate', e => {
     });
 
     e.waitUntil(respuesta);
+});
+
+self.addEventListener('fetch', e => {
+  const respuesta = caches.match(e.request).then(res => {
+    if(res){
+      return res;
+    } else {
+      return fetch(e.request).then(newRes =>{
+        return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+      });
+    }
+  });
+  e.respondWith(respuesta);
 });
